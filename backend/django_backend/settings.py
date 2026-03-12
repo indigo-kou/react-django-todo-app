3b142bc7dde89466f0a50232dc6058773105b467
huangxinyuan <xinyuan.huang@indigo.co.jp>
2026-03-12 14:00:14 +0900

    222

--- backend/django_backend/settings.py
+++ backend/django_backend/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
-import os
-
 from dotenv import load_dotenv
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,7 +55,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.getenv("DB_NAME", "react_nodejs_todo_app"),
         "USER": os.getenv("DB_USER", "root"),
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "PASSWORD": os.getenv("DB_PASSWORD", "password"),
         "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", "3306"),
         "OPTIONS": {
